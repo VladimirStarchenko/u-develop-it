f13788a607d9645be1755a803ebf1fcf25a85136
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2021-12-30 11:42:37 -0500

    issue resolved. candidates can be deleted, requested and created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,6 +25,35 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(row);
+  }
+});
+
+// DELETE a candidate
+// db.query(`DELETE * FROM candidate WHERE id = ?`, 1, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(row);
+//   }
+// });
+
+// CREATE a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+              VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 //Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
